7f715b17d7d4a62f3e4252856ece3854b62e6de9
Rick Fontein <r.fontein@hotmail.com>
2015-06-05 02:29:16 +0200

    Added some signatures for tidyness

--- Project/TestOne.hs
+++ Project/TestOne.hs
@@ -24,6 +24,8 @@ rX :: Clause
 rX = Rule ("r", Variable "X") [("p", Variable "X"),("q", Variable "X")]
 
 {-
+Example queries
+
 A query MUST be in the form of a Clause Rule, eg:
 For a true/false query:
 - Rule ("r", Constant "a") []
@@ -33,9 +35,17 @@ For a substitution
 
 Yes, every query has a redundant empty list at the end, this makes recursion in the implementation way easier.
 As a matter of fact, the list at the end is completely ignored anyway.
+
+Also, use the printResult function to get a proper representation of the result. evalOne will return internal structures.
 -}
+
+qra :: IO()
 qra = printResult $ evalOne myprogram (Rule ("r", Constant "a") [])
+qrb :: IO()
 qrb = printResult $ evalOne myprogram (Rule ("r", Constant "b") [])
+qrc :: IO()
 qrc = printResult $ evalOne myprogram (Rule ("r", Constant "c") [])
+qrx :: IO()
 qrx = printResult $ evalOne myprogram (Rule ("r", Variable "X") [])
+qry :: IO()
 qry = printResult $ evalOne myprogram (Rule ("r", Variable "Y") [])
