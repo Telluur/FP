2dccb0f64adea8fc39661b059e0846121260405a
Rick Fontein <r.fontein.95@gmail.com>
2016-04-25 17:46:43 +0200

    Completed incomplete exercises

--- Homework Series/series1.hs
+++ Homework Series/series1.hs
@@ -5,7 +5,7 @@ f x = 2*x^2 + 3*x - 5
 
 --2
 code :: Int -> Char -> Char
-code n char	
+code n char
 	| x >= 65 && x <= 90 = chr((x - 65 + n) `mod` (26) + 65)
 	| x >= 97 && x <= 122 = chr((x - 97 + n) `mod` (26) + 97)
 	| otherwise = char
@@ -13,23 +13,23 @@ code n char
 	x = ord char
 --3
 interest :: Int -> Float -> Float -> Float
-interest n a r	
+interest n a r
 	| n == 0 = a
 	| otherwise = interest (n-1) (a + a / 100 * r) r
 
 --4
 root1 :: Float -> Float -> Float -> Float
-root1 a b c 	
+root1 a b c
 	| discr a b c >= 0 = ((-b) + sqrt(b ^ 2 - 4 * a * c)) / (2 * a)
 	| otherwise = error "negative discriminant"
-				
+
 root2 :: Float -> Float -> Float -> Float
 root2 a b c
 	| discr a b c >= 0 = ((-b) - sqrt(b ^ 2 - 4 * a * c)) / (2 * a)
 	| otherwise = error "negative discriminant"
-				
+
 discr :: Float -> Float -> Float -> Float
-discr a b c = b ^ 2 - 4 * a * c 
+discr a b c = b ^ 2 - 4 * a * c
 
 --5
 extrX :: Float -> Float -> Float
@@ -41,7 +41,7 @@ extrY a b c = c - b ^ 2 / (4 * a)
 --6
 mylength :: [a] -> Int
 mylength [] = 0
-mylength (x:xs) = mylength(xs) + 1 
+mylength (x:xs) = mylength(xs) + 1
 
 mysum :: [Int] -> Int
 mysum [x] = x
@@ -51,11 +51,11 @@ myreverse :: [a] -> [a]
 myreverse [] = []
 myreverse (x:xs) = reverse xs ++ [x]
 
-mytake :: Int -> [a] -> [a]  
-mytake n _  
-    | n <= 0 = []  
-mytake _ [] = []  
-mytake n (x:xs) = x : mytake (n-1) xs 
+mytake :: Int -> [a] -> [a]
+mytake n _
+    | n <= 0 = []
+mytake _ [] = []
+mytake n (x:xs) = x : mytake (n-1) xs
 
 myelem :: (Eq a) => a -> [a] -> Bool
 myelem a [] = False
@@ -74,41 +74,38 @@ mymaximum (x:y:xs)
 	| x > y = mymaximum([x] ++ xs)
 	| otherwise = mymaximum([y] ++ xs)
 
-myzip :: [a] -> [b] -> [(a,b)]  
-myzip _ [] = []  
-myzip [] _ = []  
-myzip (x:xs) (y:ys) = (x,y):myzip xs ys  
+myzip :: [a] -> [b] -> [(a,b)]
+myzip _ [] = []
+myzip [] _ = []
+myzip (x:xs) (y:ys) = (x,y):myzip xs ys
 
 --7
-r :: Int -> Int -> [Int]
-r a d = [a + (d*i) | i <- [1..]]
+r :: (Num a, Enum a) => a -> a -> [a]
+r a d = [a + (d*i) | i <- [0..]]
 
 -- r1 is niks anders dan de !! operator? Of moet hier een eigen functie geschreven worden die over de lijst itereert?
 
-r1 :: Int -> [Int] -> Int
+r1 :: (Num a) => Int -> [a] -> a
 r1 n r = r !! n
 
-{-
-total :: Int -> Int -> Int
-total i j r
-	| i > j = error "J must be larger than I"
-	| i < 0 || j > length r = error "List out of bounds"
-	| i == j = r !! j
-	| otherwise 
--}
+total :: (Num a, Enum a) => Int -> Int -> a -> a -> a
+total i j a d = sum $ drop i $ take (j+1) (r a d)
 
 --8
-allEqual :: (Eq a) => [a] -> Bool
-allEqual [] = error "Empty list"
-allEqual [a] = True
-allEqual (x:y:xs)
-	| x == y = allEqual (y:xs)
-	| otherwise = False
 
-{-
-isAS :: [Int] -> Bool
--}
-	
+allEqual :: (Eq a) => [a] -> Bool
+allEqual [] = True
+allEqual [x] = True
+allEqual (x:y:xs) = x==y && allEqual (y:xs)
+
+allDiffs :: (Num a) => [a] -> [a]
+allDiffs [] = []
+allDiffs [x] = []
+allDiffs (x:y:xs) = (y-x) : allDiffs (y:xs)
+
+isAS :: (Num a, Eq a) => [a] -> Bool
+isAS = allEqual . allDiffs
+
 --9
 checkRows :: (Eq a) => [[a]] -> Bool
 checkRows [] = error "Empty matrix"
@@ -116,7 +113,7 @@ checkRows [a] = True
 checkRows (m:mm:ms)
 	| mylength m == mylength mm = checkRows(mm:ms)
 	| otherwise = False
-	
+
 sumRows :: [[Int]] -> [Int]
 sumRows [] = []
 sumRows (m:ms) = mysum m : sumRows ms
@@ -127,11 +124,9 @@ transpose x = (map head x) : transpose (map tail x)
 
 --Lazy and ineffective way
 sumCols :: [[Int]] -> [Int]
-sumCols m = sumRows (transpose m) 
+sumCols m = sumRows (transpose m)
 
 --Better
 sumCols2 :: [[Int]] -> [Int]
 sumCols2 ([]:_) = []
 sumCols2 x = mysum(map head x) : sumCols2 (map tail x)
-
-
